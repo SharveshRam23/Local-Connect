118bd3465086712c9f1c6caa6e280b97dd546723
sowbagyasivakumar <sowbagyasivakumar@gmail.com>
2026-02-02 17:02:30 +0000

    updated the error

--- app/src/main/java/com/example/localconnect/ui/auth/RegistrationActivity.java
+++ app/src/main/java/com/example/localconnect/ui/auth/RegistrationActivity.java
@@ -3,6 +3,7 @@ package com.example.localconnect.ui.auth;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -68,6 +69,7 @@ public class RegistrationActivity extends AppCompatActivity {
                         role = "service";
                     }
                     User user = new User(name, email, password, role, area);
+                    Log.d("RegistrationActivity", "Inserting user: " + user.toString());
                     userViewModel.insert(user);
                     Toast.makeText(RegistrationActivity.this, "Registration successful", Toast.LENGTH_SHORT).show();
                     startActivity(new Intent(RegistrationActivity.this, LoginActivity.class));
